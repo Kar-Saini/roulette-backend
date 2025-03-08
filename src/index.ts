857173d35b20e92e1922c4bed7b910fbfbb3f264
Kartik Saini <kackysaini8@gmail.com>
2025-03-08 15:14:41 +0530

    changed to wss

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import { WebSocketServer } from "ws";
+import https from "https";
+import fs from "fs";
 import UserManager from "./classes/user/UserManager";
-
-const wss = new WebSocketServer({ port: 8080 });
+const server = https.createServer({
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/kar-saini.duckdns.org/fullchain.pem"
+  ),
+  key: fs.readFileSync(
+    "/etc/letsencrypt/live/kar-saini.duckdns.org/privkey.pem"
+  ),
+});
+const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws, request) => {
+  console.log("Secure WebSocket Server Started");
+
   const url = request.url;
   const params = new URLSearchParams(url?.split("?")[1]);
   const name = params.get("name");
+
   UserManager.getInstance().addUser(ws, name || "Anonymous");
+});
+
+// Start the HTTPS server
+server.listen(8080, () => {
+  console.log("HTTPS & WSS Server running on port 8080");
 });
